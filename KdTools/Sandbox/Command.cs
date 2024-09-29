b52d5d7b166eb41f36460e93b08428122bb1412d
sgrodnik <sgrodnik@gmail.com>
2024-09-29 14:17:43 +0200

    init Sandbox

---
+++ KdTools/Sandbox/Command.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using KdTools.BASE;
+using static KdTools.Utils;
+
+namespace KdTools.Sandbox;
+
+[Transaction(TransactionMode.Manual)]
+[Regeneration(RegenerationOption.Manual)]
+class Command : IButtonCommand, IExternalCommand
+{
+    public string ButtonText => "Sandbox";
+    public string Title => "Sandbox";
+    public string Tooltip => "";
+    public Bitmap Logo { get; } = Properties.Resources.LogoTemplate;
+
+    private static void Execute(ExternalCommandData commandData)
+    {
+        new Model(commandData).DoJob();
+    }
+    
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+        try
+        {
+            LogStartCommand(Title, commandData, 0);
+            Execute(commandData);
+            LogEndCommand(Title);
+        }
+        catch (Exception e)
+        {
+            LogException(e);
+            ShowException(e);
+        }
+        return Result.Succeeded;
+    }
+}
---
+++ KdTools/Sandbox/Model.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+
+namespace KdTools.Sandbox;
+
+public class Model
+{
+    private readonly UIApplication _uiApp;
+    private readonly UIDocument _uiDoc;
+    private readonly Document _doc;
+
+    public Model(ExternalCommandData commandData)
+    {
+        _uiApp = commandData.Application;
+        _uiDoc = _uiApp.ActiveUIDocument;
+        _doc = _uiDoc.Document;
+    }
+
+    internal void DoJob()
+    {
+        // var rooms = GetRooms();
+
+        // using var t = new Transaction(_doc);
+        // t.Start(new Command().Title);
+
+        // foreach (var room in rooms)
+        // {
+        //     CreateCeiling(room as SpatialElement);
+        //     _resultCounter++;
+        // }
+
+        // t.SetName($"{t.GetName()} ({_resultCounter})");
+        // t.Commit();
+    }
+}
